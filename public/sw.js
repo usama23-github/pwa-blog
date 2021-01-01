15df845cefa71697a0567497d6939442f4292965
unknown <ua92989@gmail.com>
2021-01-01 14:01:18 +0500

    pwa

---
+++ public/sw.js
@@ -0,0 +1,29 @@
+let cacheData = "appV1";
+
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheData).then((cache) => {
+      cache.addAll([
+        "/static/js/bundle.js",
+        "/static/js/main.chunk.js",
+        "/static/js/0.chunk.js",
+        "/index.html",
+        "/",
+      ]);
+    })
+  );
+});
+
+this.addEventListener("fetch", (event) => {
+  if (!navigator.online) {
+    event.respondWith(
+      caches.match(event.request).then((result) => {
+        if (result) {
+          return result;
+        }
+        let requestUrl = event.request.clone();
+        return fetch(requestUrl);
+      })
+    );
+  }
+});
